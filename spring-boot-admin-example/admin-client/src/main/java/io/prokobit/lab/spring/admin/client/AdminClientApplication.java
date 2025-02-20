b995f3f99dbfcd8e53992a017f8d55fc395029d7
Przemysław Kobylański <104228966+prokobit@users.noreply.github.com>
2025-02-20 22:44:58 +0100

    Init (#1)

---
+++ spring-boot-admin-example/admin-client/src/main/java/io/prokobit/lab/spring/admin/client/AdminClientApplication.java
@@ -0,0 +1,12 @@
+package io.prokobit.lab.spring.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdminClientApplication.class, args);
+  }
+}
