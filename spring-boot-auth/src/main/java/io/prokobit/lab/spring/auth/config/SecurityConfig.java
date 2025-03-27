89956ae60daa5a25a31af41973384f29518ddc90
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-03-27 01:35:57 +0100

    Spring Boot OAuth2

---
+++ spring-boot-auth/src/main/java/io/prokobit/lab/spring/auth/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package io.prokobit.lab.spring.auth.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+@Profile("!test")
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // spotless:off
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/login.html", "/error", "/webjars/**").permitAll()
+            .anyRequest().authenticated())
+//        .exceptionHandling(e ->
+//            e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//        )
+        .oauth2Login((login) -> login
+            .loginPage("/login.html")
+            .redirectionEndpoint((endpoint) -> endpoint.baseUri("/login/oauth2/callback/*"))
+            .defaultSuccessUrl("/")
+        )
+        .logout(l -> l
+            .logoutSuccessUrl("/").permitAll()
+        )
+//        .sessionManagement(s -> s
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        )
+        .build();
+    // spotless:on
+  }
+}
---
+++ spring-boot-auth/src/test/java/io/prokobit/lab/spring/auth/SpringBootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prokobit.lab.spring.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SpringBootAuthApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
