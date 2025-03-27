42dfc622876d9cb721d31e7e1b0032c790f1cd01
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-03-27 01:35:57 +0100

    Spring Boot OAuth2

---
+++ spring-boot-auth/src/main/java/io/prokobit/lab/spring/auth/web/UserController.java
@@ -0,0 +1,16 @@
+package io.prokobit.lab.spring.auth.web;
+
+import java.util.Map;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @GetMapping("/user")
+  public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+    return Map.of("name", principal.getAttribute("name"), "email", principal.getAttribute("email"));
+  }
+}
