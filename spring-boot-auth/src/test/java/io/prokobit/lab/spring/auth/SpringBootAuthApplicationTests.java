34200a421773dc307b6482c8bf90e8625bef6928
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-06-04 21:22:48 +0200

    Fix build

--- spring-boot-auth/src/test/java/io/prokobit/lab/spring/auth/SpringBootAuthApplicationTests.java
+++ spring-boot-auth/src/test/java/io/prokobit/lab/spring/auth/SpringBootAuthApplicationTests.java
@@ -2,10 +2,8 @@ package io.prokobit.lab.spring.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class SpringBootAuthApplicationTests {
 
   @Test
